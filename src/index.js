de9a1b78c6ed5cc87d9b76a041717c4160d1d227
markthree <1801982702@qq.com>
2023-08-15 19:07:07 +0800

    chore: update deps

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ const { resolve } = require("path");
 const { cyan } = require("kolorist");
 const { consola } = require("consola");
 const { copy } = require("fast-cpy");
-const { cwd } = require("process");
 const { select, input } = require("@inquirer/prompts");
 
 const log = consola.withTag("n-init-project");
