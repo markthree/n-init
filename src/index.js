e5e80a1275ce56ee551493c3b607e73162838201
markthree <1801982702@qq.com>
2023-09-01 20:57:56 +0800

    feat: add autoInstall

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const { resolve } = require("path");
 const { cyan } = require("kolorist");
 const { consola } = require("consola");
 const { copy } = require("fast-cpy");
+const { existsSync } = require("fs");
+const { execSync } = require("child_process");
 const { select, input } = require("@inquirer/prompts");
 
 const log = consola.withTag("n-init-project");
@@ -19,20 +21,50 @@ async function init() {
   const choices = projects.map((p) => ({ name: p, value: p }));
 
   const answer = await select({
-    message: "Choose your template?",
+    message: "选择你的模板?",
     choices,
   });
 
-  const dest = await input({
+  let dest = await input({
     default: `${answer}-starter`,
-    message: "input your project name",
+    message: "输入你的项目名",
+  });
+
+  const _cwd = process.cwd();
+
+  dest = resolve(_cwd, dest);
+
+  if (existsSync(dest)) {
+    log.error("你的项目已存在，请换一个项目名");
+    return;
+  }
+
+  const pm = await select({
+    message: "选择你使用的包管理器?",
+    choices: [{
+      name: "npm",
+      value: "npm install",
+    }, {
+      name: "yarn",
+      value: "yarn",
+    }, {
+      name: "pnpm",
+      value: "pnpm install",
+    }],
   });
 
   const src = resolve(projectsDir, answer);
 
   await copy(src, dest);
 
-  log.success(`Successfully generated → ${cyan(dest)}`);
+  log.success(`生成项目成功 → ${cyan(dest)}`);
+
+  execSync(pm, {
+    stdio: "inherit",
+    cwd: _cwd,
+  });
+
+  log.success(`安装项目成功`);
 }
 
 init();
